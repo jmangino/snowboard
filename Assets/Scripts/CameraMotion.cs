70f790710ddb5609a447ed1be9c687a7025071c7
Joseph Mangino <joseph@mangino.co>
2015-03-14 12:28:28 -0400

    initial commit

---
+++ Assets/Scripts/CameraMotion.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMotion : MonoBehaviour {
+
+	public GameObject player;
+	
+	public float distance_away;
+	public float distance_up;
+	private Transform follow; //this is the Player's transform
+	private Vector3 target;
+	public float smooth = 10;
+
+	
+	// Use this for initialization
+	void Start () {
+		follow = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void LateUpdate(){
+		if (player != null) {
+			//decent but z rotation causes camera to fly forward and backward through object
+			//target = player.transform.position + Vector3.up * distance_up - follow.forward * distance_away;
+
+			Vector3 forward = player.GetComponent<PlayerMove>().getLookDirection();
+			//rotate (0,0,1) normal forward around x axis by rotation of player's x rotation
+			//Vector3 forward = Quaternion.AngleAxis(follow.eulerAngles.x,new Vector3(1,0,0)) * new Vector3(0,0,1);
+			//ROTATE 0,0,1 AROUND X FOR CAM POS INDEP OF Y,Z
+			target = player.transform.position + Vector3.up * distance_up - forward * distance_away;
+			transform.position = Vector3.Lerp(target, transform.position, smooth * Time.deltaTime);
+			transform.LookAt(player.transform);
+
+		}
+	}
+
+	void CopyTransform(Transform dst , Transform src) {
+		dst.localPosition = src.localPosition;
+		dst.localRotation = src.localRotation;
+		dst.localScale = src.localScale;
+	}
+
+
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerMove : MonoBehaviour {
+	private float dx =0, dy=0, dz=0;
+	private float ddx = 0f, ddy = -0.1f, ddz = 0f;
+	private float dmax = 100;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+
+	}
+
+
+
+	//apparently raycasts need to happen after fixed update so do that here?
+	void LateUpdate(){
+		RaycastHit to_ground;
+		//ISSUE: transform.position is player center not bottom
+		//ISSUE: move sideways then downs so always grounded.
+		Physics.Raycast (transform.position, -Vector3.up, out to_ground);
+		//in air
+		if (to_ground.distance > 3.2) {
+			dy += ddy;
+			//will hit ground
+			if (to_ground.distance < dy) {
+				dy = to_ground.distance + 2.5f;
+			}
+		} else { //on ground
+			//method call here only makes sense for grounded objects
+			dy = 0;
+			findLocalGrad();
+		}
+		transform.Translate (new Vector3 (dx, dy, dz));
+
+		Debug.Log ("local grad dx:"+dx+", dz:"+dz);
+
+
+	}
+
+	void findLocalGrad(){
+		float delta = 0.15f;
+		Vector3 zshift = new Vector3(0f,0f,delta);
+		Vector3 xshift = new Vector3 (delta, 0f, 0f);
+		Vector3 down = new Vector3(0f,-1f,0f);
+		Vector3 dleft = down + zshift;
+		Vector3 dright = down - zshift;
+	
+		RaycastHit r1, r2;
+		Physics.Raycast (transform.position, dleft, out r1);
+		Physics.Raycast (transform.position, dright, out r2);
+
+		Debug.Log ("z distances: "+r1.distance+", "+r2.distance);
+
+		dz = r1.distance - r2.distance;
+
+		Vector3 dforward = down + xshift;
+		Vector3 dback = down - xshift;
+
+		Physics.Raycast (transform.position, dforward, out r1);
+		Physics.Raycast (transform.position, dback, out r2);
+		
+		dx = r1.distance - r2.distance;
+
+		Debug.DrawRay (transform.position, dleft, Color.blue);
+		Debug.DrawRay (transform.position, dforward, Color.green);
+		Debug.DrawRay (transform.position, dright, Color.cyan);
+		Debug.DrawRay (transform.position, dback, Color.red);
+
+	}
+
+	public Vector3 getDirection (){
+		return new Vector3(dx,dy,dz);
+	}
+
+	public Vector3 getLookDirection (){
+		return new Vector3(dx,dy/3,dz);
+	}
+
+
+
+
+}
