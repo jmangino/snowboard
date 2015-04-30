3d3bf29b639b407c7bfd3c9b6417dc708e8ce571
Joseph Mangino <joseph@mangino.co>
2015-04-30 08:53:26 -0400

    added y clip, turning

--- Assets/Scripts/CameraMotion.cs
+++ Assets/Scripts/CameraMotion.cs
@@ -7,14 +7,15 @@ public class CameraMotion : MonoBehaviour {
 	
 	public float distance_away;
 	public float distance_up;
-	private Transform follow; //this is the Player's transform
 	private Vector3 target;
 	public float smooth = 10;
+	private Vector3 forward;
+	private Vector3 up1 = new Vector3(0,1,0);
 
 	
 	// Use this for initialization
 	void Start () {
-		follow = GameObject.FindGameObjectWithTag ("Player").transform;
+
 	}
 	
 	// Update is called once per frame
@@ -24,25 +25,17 @@ public class CameraMotion : MonoBehaviour {
 	
 	void LateUpdate(){
 		if (player != null) {
-			//decent but z rotation causes camera to fly forward and backward through object
-			//target = player.transform.position + Vector3.up * distance_up - follow.forward * distance_away;
 
-			Vector3 forward = player.GetComponent<PlayerMove>().getLookDirection();
-			//rotate (0,0,1) normal forward around x axis by rotation of player's x rotation
-			//Vector3 forward = Quaternion.AngleAxis(follow.eulerAngles.x,new Vector3(1,0,0)) * new Vector3(0,0,1);
+			forward = player.GetComponent<PlayerMove>().getLookDirection();
 			//ROTATE 0,0,1 AROUND X FOR CAM POS INDEP OF Y,Z
 			target = player.transform.position + Vector3.up * distance_up - forward * distance_away;
 			transform.position = Vector3.Lerp(target, transform.position, smooth * Time.deltaTime);
-			transform.LookAt(player.transform);
+			//look @ player center not board
+			transform.LookAt(player.transform.position + up1);
 
 		}
 	}
-
-	void CopyTransform(Transform dst , Transform src) {
-		dst.localPosition = src.localPosition;
-		dst.localRotation = src.localRotation;
-		dst.localScale = src.localScale;
-	}
+	
 
 
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,14 +3,49 @@ using System.Collections;
 
 
 public class PlayerMove : MonoBehaviour {
+	//movement
+	private float ddmax = 0.35f;
 	private float dx =0, dy=0, dz=0;
 	private float ddx = 0f, ddy = -0.1f, ddz = 0f;
 	private float dmax = 100;
-	public float SPEEDUP = 0.02f;
-	public float DRAG = 0.01f; //add other kinds of drag?
+	//angular
+	private float dtheta = 0.81f;
+
+	//correction
+	private float ycorrect = 0.0f;
+	private float yclipup = 0.3f;
+	//FIXME causing jumps when -dy is too great, immediately slowing in air (keep track of dy?)
+	private float yclipdown = 0.2f;
+
+	//acceleration
+	public float SPEEDUP = 1f; //set elsewhere
+	public float DRAG = 0.001f; //add other kinds of drag?
 
 	int ground_mask = 1<<8;
 
+	//controls
+	private bool turnleft = false;
+	private bool turnright = false;
+	private bool brake = false;
+
+	//flags
+	private bool onground = false;
+
+	//grad
+	private static float delta = 0.1f;
+	//ADJUST xandz shift to account for turned player
+	private static Vector3 zshift = new Vector3 (0, 0, delta);
+	private static Vector3 xshift = new Vector3 (delta, 0, 0);
+	private static Vector3 down = new Vector3(0f,-1f,0f);
+
+	Vector3 dleft = down + zshift;
+	Vector3 dright = down - zshift;
+	Vector3 dforward = down + xshift;
+	Vector3 dback = down - xshift;
+
+	RaycastHit r1, r2;
+	RaycastHit to_ground;
+	
 
 
 	// Use this for initialization
@@ -31,68 +66,77 @@ public class PlayerMove : MonoBehaviour {
 
 	//Raycast after fixed update
 	void LateUpdate(){
-		RaycastHit to_ground;
+		useInputs ();
 		//ISSUE: move sideways then downs so always grounded.
 		Physics.Raycast (transform.position, -Vector3.up, out to_ground,100f, ground_mask);
 		//in air, NO HIT -> falling
-		if (to_ground.distance > 0.1) {
-			dy += ddy;
+		if (to_ground.distance > 0.2f) {
+			onground = false;
+			//dy = dy + ddy * dt
+			dy += ddy * Time.deltaTime;
 			//will hit ground
-			if (to_ground.distance < dy) {
-				dy = to_ground.distance + 0.1f;
+			if ( to_ground.distance - dy < 0) {
+				dy = to_ground.distance + 0.01f;
 			}
 		} else { //on ground
-			//method call here only makes sense for grounded objects
+			onground = true;
 			dy = 0;
 			findLocalGrad();
-			dx += ddx;
-			dz += ddz;
+			dx += ddx * Time.deltaTime;
+			dz += ddz * Time.deltaTime;
+
+			//set local x,z rotation to reflect grad
+
 			//factor in max speed and drag?
 		}
-		transform.Translate (new Vector3 (dx, dy, dz));
-		useInputs ();
-		Debug.Log ("local grad dx:"+dx+", dz:"+dz);
+		transform.Translate (dx, dy, dz);
+		//correct y
+		ycorrect = 0;
+		if(onground){
+			Physics.Raycast (transform.position, -Vector3.up, out to_ground,10f, ground_mask);
+			ycorrect = to_ground.distance;
+			//gravity will naturally clip back to ground
+			if(ycorrect < yclipdown && ycorrect != 0){
 
+				ycorrect *= -1;
+			}else {
+				//clip up, fix this so I don't have to cast twice
+				Physics.Raycast (transform.position, Vector3.up, out to_ground,10f, ground_mask);
+				ycorrect = Mathf.Clamp(ycorrect, 0, yclipup);
+			}
+		}
+		transform.Translate (0, ycorrect, 0);
 
 	}
 
 	void findLocalGrad(){
-		float delta = 0.1f;
-		Vector3 zshift = new Vector3(0f,0f,delta);
-		Vector3 xshift = new Vector3 (delta, 0f, 0f);
-		Vector3 down = new Vector3(0f,-1f,0f);
-		Vector3 dleft = down + zshift;
-		Vector3 dright = down - zshift;
 		Vector3 position = transform.position; position.y += 1;
-	
-		RaycastHit r1, r2;
+		//TODO recalculate this by shooting straight down from sides, then use rise over run, currently approximating
 		Physics.Raycast (position, dleft, out r1, 10f, ground_mask);
 		Physics.Raycast (position, dright, out r2, 10f, ground_mask);
-
-		Debug.Log ("z distances: "+r1.distance+", "+r2.distance);
-
-		ddz = r1.distance - r2.distance;
+		ddz = (r1.distance - r2.distance)/(delta*2);
+		//limit acceleration to no faster than gravity
+		ddz = Mathf.Clamp (ddz, -1 * ddmax, ddmax);
 		ddz *= SPEEDUP;
-
-		Vector3 dforward = down + xshift;
-		Vector3 dback = down - xshift;
 
 		Physics.Raycast (position, dforward, out r1, 10f, ground_mask);
 		Physics.Raycast (position, dback, out r2, 10f, ground_mask);
-		
-		ddx = r1.distance - r2.distance;
+		ddx = (r1.distance - r2.distance)/(delta*2);
+		//limit acceleration to no faster than gravity
+		ddx = Mathf.Clamp (ddx, -1 * ddmax, ddmax);
 		ddx *= SPEEDUP;
-
-  
 
 	}
 
+	//TODO fix the directions to make them turn independent (90 deg sideways is messing up a lot)
 	public Vector3 getDirection (){
 		return new Vector3(dx,dy,dz);
 	}
 
 	public Vector3 getLookDirection (){
-		return new Vector3(dx,dy/3,dz);
+		Vector3 v = new Vector3 (dx, 0, dz);
+		v.Normalize ();
+		return v;
 	}
 
 
@@ -103,13 +147,54 @@ public class PlayerMove : MonoBehaviour {
 	private void useInputs(){
 		//ISSUE: register inputs to axis?-----------
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			//rotate dx,dz left
+			if(onground){
+				turnleft = true;
+				turnright = false;
+			}
 		}
 		if (Input.GetKeyDown (KeyCode.RightArrow)) {
 			//rotate dx,dz right
+			if(onground){
+				turnright = true;
+				turnleft = false;
+			}
 		}
 		if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			//slow down
+			if(onground){
+				brake = true;
+			}
+		}
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			turnleft = false;
+		}
+
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			turnright = false;
+		}
+		if (Input.GetKeyUp (KeyCode.DownArrow)) {
+			brake = false;
+		}
+
+		//use inputs
+		//TODO add theta acceleration so turning doesn't seem so jerky
+		//TODO turning isn't perfectly aligned with player rotation, rotate player towards movement vector? 
+		if (turnleft) {
+			float theta = dtheta * Time.deltaTime;
+			float tempdx = dx;
+			dx = Mathf.Cos(theta) * dx - Mathf.Sin(theta) * dz;
+			dz = Mathf.Cos (theta) * dz + Mathf.Sin(theta) * tempdx;
+			//transform.Rotate(0,-theta*360.0f,0);
+		}
+		if (turnright) {
+			float theta = -1 * dtheta * Time.deltaTime;
+			float tempdx = dx;
+			dx = Mathf.Cos(theta) * dx - Mathf.Sin(theta) * dz;
+			dz = Mathf.Cos (theta) * dz + Mathf.Sin(theta) * tempdx;
+			//transform.Rotate(0,-theta*360.0f,0);
+		}
+		if (brake) {
+			//slow down (add drag)??
 		}
 
 	}
